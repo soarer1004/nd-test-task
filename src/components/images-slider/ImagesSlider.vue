b3f2c2fa7f8f2f29316e7c47c250c80d3eaf3fdb
Anton Sylka <soarer1004@yandex.ru>
2020-02-02 17:49:25 +0800

    Fix buttons styles

--- src/components/images-slider/ImagesSlider.vue
+++ src/components/images-slider/ImagesSlider.vue
@@ -1,13 +1,21 @@
 <template>
-    <div class="images-slider-container">
-      <button class="image-change left" @click="showPreviousImage"> < </button>
-      <transition :name="transitionName">
-        <image-view :image="activeImage"
-                    :key="activeImage.name"
-                    :displayType="imagesDisplayType"/>
-      </transition>
-      <button class="image-change right" @click="showNextImage"> > </button>
-    </div>
+  <div class="images-slider-container">
+    <template v-if="imagesList.length > 1">
+      <button class="image-change left"
+              @click="showPreviousImage">
+        <
+      </button>
+      <button class="image-change right"
+              @click="showNextImage"> >
+      </button>
+    </template>
+
+    <transition :name="transitionName">
+      <image-view :image="activeImage"
+                  :key="activeImage.name"
+                  :displayType="imagesDisplayType"/>
+    </transition>
+  </div>
 </template>
 
 <script lang="ts" src="./ImagesSlider.ts"></script>
