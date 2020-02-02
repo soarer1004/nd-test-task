76308150092ac52b0d15411bf12a87e07c40effe
Anton Sylka <soarer1004@yandex.ru>
2020-02-03 07:45:44 +0800

    Implement settings saving after page reload

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 import getters from '@/store/root/getters';
 import mutations from '@/store/root/mutations';
@@ -11,5 +12,12 @@ Vue.use(Vuex);
 export default new Vuex.Store({
     getters,
     mutations,
+    plugins: [
+        createPersistedState({
+            paths: [
+                'imagesDisplayType',
+            ],
+        }),
+    ],
     state: new RootState(),
 });
