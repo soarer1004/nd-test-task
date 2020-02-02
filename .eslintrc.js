67dc5acc3ea59e95bc44e8c21089117c726aaec1
Anton Sylka <soarer1004@yandex.ru>
2020-02-02 10:36:16 +0800

    Improve eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,29 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript'
+    '@vue/standard'
+    // 'plugin:@typescript-eslint/eslint-recommended',
+    // 'plugin:@typescript-eslint/recommended'
+  ],
+  plugins: [
+    '@typescript-eslint'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'error',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
-  }
+  },
+  overrides: [
+    {
+      files: ['*.ts'],
+      rules: {
+        'semi': 'off',
+        'comma-dangle': ['error', 'always-multiline'],
+        '@typescript-eslint/semi': ['error'],
+        '@typescript-eslint/member-delimiter-style': ['error']
+      }
+    }
+  ]
 }
